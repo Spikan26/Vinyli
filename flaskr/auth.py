1985dc8bb9d1c6bbad90052815161e1dfcdd6937
Spikan26 <durieuxalexandre26@gmail.com>
2020-07-17 10:31:16 +0200

    Reformatting with Black

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -1,97 +1,106 @@
 import functools
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, session, url_for
+    Blueprint,
+    flash,
+    g,
+    redirect,
+    render_template,
+    request,
+    session,
+    url_for,
 )
 from werkzeug.security import check_password_hash, generate_password_hash
 from flaskr.db import get_db
 
 
-bp = Blueprint('auth', __name__, url_prefix='/auth')
+bp = Blueprint("auth", __name__, url_prefix="/auth")
 
 
-@bp.route('/register', methods=('GET', 'POST'))
+@bp.route("/register", methods=("GET", "POST"))
 def register():
-    if request.method == 'POST':
-        email = request.form['email']
-        mdp = request.form['mdp']
-        nom = request.form['nom']
-        prenom = request.form['prenom']
+    if request.method == "POST":
+        email = request.form["email"]
+        mdp = request.form["mdp"]
+        nom = request.form["nom"]
+        prenom = request.form["prenom"]
         db = get_db()
         error = None
 
         if not email:
-            error = 'Email requis.'
+            error = "Email requis."
         elif not mdp:
-            error = 'Mot de passe requis.'
-        elif db.execute(
-            'SELECT idClient FROM Client WHERE email = ?', (email,)
-        ).fetchone() is not None:
-            error = 'Email {} déjà renseigné.'.format(email)
+            error = "Mot de passe requis."
+        elif (
+            db.execute(
+                "SELECT idClient FROM Client WHERE email = ?", (email,)
+            ).fetchone()
+            is not None
+        ):
+            error = "Email {} déjà renseigné.".format(email)
 
         if error is None:
             db.execute(
-                'INSERT INTO Client (email, mdp, nom, prenom) VALUES (?, ?, ?, ?)',
-                (email, generate_password_hash(mdp), nom, prenom)
+                "INSERT INTO Client (email, mdp, nom, prenom) VALUES (?, ?, ?, ?)",
+                (email, generate_password_hash(mdp), nom, prenom),
             )
             db.commit()
-            return redirect(url_for('auth.login'))
+            return redirect(url_for("auth.login"))
 
         flash(error)
 
-    return render_template('auth/register.html')
+    return render_template("auth/register.html")
 
 
-@bp.route('/login', methods=('GET', 'POST'))
+@bp.route("/login", methods=("GET", "POST"))
 def login():
-    if request.method == 'POST':
-        email = request.form['email']
-        mdp = request.form['mdp']
+    if request.method == "POST":
+        email = request.form["email"]
+        mdp = request.form["mdp"]
         db = get_db()
         error = None
-        client = db.execute(
-            'SELECT * FROM Client WHERE email = ?', (email,)
-        ).fetchone()
+        client = db.execute("SELECT * FROM Client WHERE email = ?", (email,)).fetchone()
 
         if client is None:
-            error = 'Email incorrect.'
-        elif not check_password_hash(client['mdp'], mdp):
-            error = 'Mot de passe incorrect.'
+            error = "Email incorrect."
+        elif not check_password_hash(client["mdp"], mdp):
+            error = "Mot de passe incorrect."
 
         if error is None:
             session.clear()
-            session['user_id'] = client['idClient']
-            return redirect(url_for('index'))
+            session["user_id"] = client["idClient"]
+            return redirect(url_for("index"))
 
         flash(error)
 
-    return render_template('auth/login.html')
+    return render_template("auth/login.html")
 
 
 @bp.before_app_request
 def load_logged_in_user():
-    user_id = session.get('user_id')
+    user_id = session.get("user_id")
 
     if user_id is None:
         g.user = None
     else:
-        g.user = get_db().execute(
-            'SELECT * FROM Client WHERE idClient = ?', (user_id,)
-        ).fetchone()
+        g.user = (
+            get_db()
+            .execute("SELECT * FROM Client WHERE idClient = ?", (user_id,))
+            .fetchone()
+        )
 
 
-@bp.route('/logout')
+@bp.route("/logout")
 def logout():
     session.clear()
-    return redirect(url_for('index'))
+    return redirect(url_for("index"))
 
 
 def login_required(view):
     @functools.wraps(view)
     def wrapped_view(**kwargs):
         if g.user is None:
-            return redirect(url_for('auth.login'))
+            return redirect(url_for("auth.login"))
 
         return view(**kwargs)
 
     return wrapped_view
-
--- flaskr/index.py
+++ flaskr/index.py
@@ -1,67 +1,69 @@
-from flask import (
-    Blueprint, flash, g, redirect, render_template, request, url_for
-)
+from flask import Blueprint, flash, g, redirect, render_template, request, url_for
 from werkzeug.exceptions import abort
 
 from flaskr.auth import login_required
 from flaskr.db import get_db
 
-bp = Blueprint('index', __name__)
+bp = Blueprint("index", __name__)
 
 
-@bp.route('/')
+@bp.route("/")
 def index():
     db = get_db()
     produits = db.execute(
-        'SELECT p.idProduit, p.titre, p.annee_sortie, p.prix, p.quantite, p.image, p.description'
-        ' FROM Produit p '
+        "SELECT p.idProduit, p.titre, p.annee_sortie, p.prix, p.quantite, p.image, p.description"
+        " FROM Produit p "
     ).fetchall()
-    return render_template('index.html', produits=produits)
+    return render_template("index.html", produits=produits)
 
 
-@bp.route('/create', methods=('GET', 'POST'))
+@bp.route("/create", methods=("GET", "POST"))
 @login_required
 def create():
-    if request.method == 'POST':
-        titre = request.form['titre']
-        annee_sortie = request.form['annee_sortie']
-        prix = request.form['prix']
-        quantite = request.form['quantite']
-        image = request.form['image']
-        description = request.form['description']
+    if request.method == "POST":
+        titre = request.form["titre"]
+        annee_sortie = request.form["annee_sortie"]
+        prix = request.form["prix"]
+        quantite = request.form["quantite"]
+        image = request.form["image"]
+        description = request.form["description"]
         error = None
 
         if not titre:
-            error = 'Un titre est requis.'
+            error = "Un titre est requis."
 
         if not prix:
-            error = 'Un prix est requis.'
+            error = "Un prix est requis."
 
         if not quantite:
-            error = 'Une quantite est requise.'
+            error = "Une quantite est requise."
 
         if error is not None:
             flash(error)
         else:
             db = get_db()
             db.execute(
-                'INSERT INTO Produit (titre, annee_sortie, prix, quantite, image, description)'
-                ' VALUES (?, ?, ?, ?, ?, ?)',
-                (titre, annee_sortie, prix, quantite, image, description)
+                "INSERT INTO Produit (titre, annee_sortie, prix, quantite, image, description)"
+                " VALUES (?, ?, ?, ?, ?, ?)",
+                (titre, annee_sortie, prix, quantite, image, description),
             )
             db.commit()
-            return redirect(url_for('index'))
+            return redirect(url_for("index"))
 
-    return render_template('create.html')
+    return render_template("create.html")
 
 
 def get_produit(id):
-    produit = get_db().execute(
-        'SELECT p.idProduit, p.titre, p.annee_sortie, p.prix, p.quantite, p.image, p.description'
-        ' FROM Produit p '
-        ' WHERE p.idProduit = ?',
-        (id,)
-    ).fetchone()
+    produit = (
+        get_db()
+        .execute(
+            "SELECT p.idProduit, p.titre, p.annee_sortie, p.prix, p.quantite, p.image, p.description"
+            " FROM Produit p "
+            " WHERE p.idProduit = ?",
+            (id,),
+        )
+        .fetchone()
+    )
 
     if produit is None:
         abort(404, "Le produit d'id {0} n'existe pas.".format(id))
@@ -69,49 +71,49 @@ def get_produit(id):
     return produit
 
 
-@bp.route('/<int:idProduit>/update', methods=('GET', 'POST'))
+@bp.route("/<int:idProduit>/update", methods=("GET", "POST"))
 @login_required
 def update(idProduit):
     produit = get_produit(idProduit)
 
-    if request.method == 'POST':
-        titre = request.form['titre']
-        annee_sortie = request.form['annee_sortie']
-        prix = request.form['prix']
-        quantite = request.form['quantite']
-        image = request.form['image']
-        description = request.form['description']
+    if request.method == "POST":
+        titre = request.form["titre"]
+        annee_sortie = request.form["annee_sortie"]
+        prix = request.form["prix"]
+        quantite = request.form["quantite"]
+        image = request.form["image"]
+        description = request.form["description"]
         error = None
 
         if not titre:
-            error = 'Un titre est requis.'
+            error = "Un titre est requis."
 
         if not prix:
-            error = 'Un prix est requis.'
+            error = "Un prix est requis."
 
         if not quantite:
-            error = 'Une quantite est requise.'
+            error = "Une quantite est requise."
 
         if error is not None:
             flash(error)
         else:
             db = get_db()
             db.execute(
-                'UPDATE Produit SET titre = ?, annee_sortie = ?, prix = ?, quantite = ?, image = ?, description = ?'
-                ' WHERE idProduit = ?',
-                (titre, annee_sortie, prix, quantite, image, description, idProduit)
+                "UPDATE Produit SET titre = ?, annee_sortie = ?, prix = ?, quantite = ?, image = ?, description = ?"
+                " WHERE idProduit = ?",
+                (titre, annee_sortie, prix, quantite, image, description, idProduit),
             )
             db.commit()
-            return redirect(url_for('index'))
+            return redirect(url_for("index"))
 
-    return render_template('update.html', produit=produit)
+    return render_template("update.html", produit=produit)
 
 
-@bp.route('/<int:idProduit>/delete', methods=('POST',))
+@bp.route("/<int:idProduit>/delete", methods=("POST",))
 @login_required
 def delete(idProduit):
     get_produit(idProduit)
     db = get_db()
-    db.execute('DELETE FROM Produit WHERE idProduit = ?', (idProduit,))
+    db.execute("DELETE FROM Produit WHERE idProduit = ?", (idProduit,))
     db.commit()
-    return redirect(url_for('index'))
+    return redirect(url_for("index"))
