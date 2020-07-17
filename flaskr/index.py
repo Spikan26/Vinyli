191be1fe189b8251c2c8db6a74f3e47e13157767
Spikan26 <durieuxalexandre26@gmail.com>
2020-07-17 10:29:35 +0200

    Delete + Update + various changes
    - Fonctionnement de la méthode Delete avec confirmation.
    - Modification possible sur les produit.
    - Les 2 méthodes ne sont disponible que lorsqu'un utilisateur est connecté.
    - Le logo du site ramène à l'index

--- flaskr/index.py
+++ flaskr/index.py
@@ -32,7 +32,13 @@ def create():
         error = None
 
         if not titre:
-            error = 'Title is required.'
+            error = 'Un titre est requis.'
+
+        if not prix:
+            error = 'Un prix est requis.'
+
+        if not quantite:
+            error = 'Une quantite est requise.'
 
         if error is not None:
             flash(error)
@@ -49,8 +55,8 @@ def create():
     return render_template('create.html')
 
 
-def get_post(id, check_author=True):
-    post = get_db().execute(
+def get_produit(id):
+    produit = get_db().execute(
         'SELECT p.idProduit, p.titre, p.annee_sortie, p.prix, p.quantite, p.image, p.description'
         ' FROM Produit p '
         ' WHERE p.idProduit = ?',
@@ -60,14 +66,52 @@ def get_post(id, check_author=True):
     if produit is None:
         abort(404, "Le produit d'id {0} n'existe pas.".format(id))
 
-    return post
+    return produit
 
 
-@bp.route('/<int:id>/delete', methods=('POST',))
+@bp.route('/<int:idProduit>/update', methods=('GET', 'POST'))
 @login_required
-def delete(id):
-    get_post(id)
+def update(idProduit):
+    produit = get_produit(idProduit)
+
+    if request.method == 'POST':
+        titre = request.form['titre']
+        annee_sortie = request.form['annee_sortie']
+        prix = request.form['prix']
+        quantite = request.form['quantite']
+        image = request.form['image']
+        description = request.form['description']
+        error = None
+
+        if not titre:
+            error = 'Un titre est requis.'
+
+        if not prix:
+            error = 'Un prix est requis.'
+
+        if not quantite:
+            error = 'Une quantite est requise.'
+
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'UPDATE Produit SET titre = ?, annee_sortie = ?, prix = ?, quantite = ?, image = ?, description = ?'
+                ' WHERE idProduit = ?',
+                (titre, annee_sortie, prix, quantite, image, description, idProduit)
+            )
+            db.commit()
+            return redirect(url_for('index'))
+
+    return render_template('update.html', produit=produit)
+
+
+@bp.route('/<int:idProduit>/delete', methods=('POST',))
+@login_required
+def delete(idProduit):
+    get_produit(idProduit)
     db = get_db()
-    db.execute('DELETE FROM Produit WHERE idProduit = ?', (id,))
+    db.execute('DELETE FROM Produit WHERE idProduit = ?', (idProduit,))
     db.commit()
     return redirect(url_for('index'))
